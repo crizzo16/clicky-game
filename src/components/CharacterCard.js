14870ea9e993bd98d87d65ade61e795abbb64bb8
Celia Rizzo <crizzo16@users.noreply.github.com>
2018-10-15 20:54:41 -0700

    Comments

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -9,7 +9,7 @@ const CharacterCard = props => {
     <div onClick={() => props.selectChar(props.id)} className="card hoverable card-size" data-id={props.id}>
       <div className="card-image img-width">
         <img src={props.image} alt={props.name} />
-        {/*<span className="card-title">{props.name}</span>*/}
+        <span className="card-title">{props.name}</span>
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -13,11 +13,11 @@ class Game extends Component {
     guessed: [],
     options: chars,
     win: false,
-    lose: false,
-    chars
+    lose: false
   };
 
   selectChar = id => {
+    // Only do this is the user hasn't won or lost yet
     if (!this.state.lose && !this.state.win) {
       if (this.state.guessed.includes(id)) {
         // User has lost
@@ -26,15 +26,20 @@ class Game extends Component {
         if (this.state.currPoints > this.state.topPoints) {
           this.setState({ topPoints: this.state.currPoints });
         }
-
-        // Offer option for game reset
-        //this.reset();
       } else {
+        // Add the clicked character's id to the list of guessed characters
         const guessed = this.state.guessed.concat(id);
+        // Update the guessed array and the current amount of points
+        // I know the setting of currPoints is weird but it's the only way it works
         this.setState({ guessed, currPoints: this.state.guessed.length + 1 });
+        // Shuffle the characters so the user gets confused
         this.shuffle();
-
-        if (this.state.guessed.length >= (this.state.options.length-1)) {
+        // If their current score is better than their best score, show it
+        if (this.state.currPoints >= this.state.topPoints) {
+          this.setState({ topPoints: this.state.currPoints + 1 });
+        }
+        // If they've guessed all the characters, alert them of their feat of memory!
+        if (this.state.guessed.length >= this.state.options.length - 1) {
           this.setState({ win: true });
         }
       }
@@ -44,24 +49,28 @@ class Game extends Component {
   shuffle() {
     let rand, temp, i;
     let options = this.state.options;
+    // Iterate through array, switching with random indexes
     for (i = options.length - 1; i > 0; i--) {
       rand = Math.floor(Math.random() * (i + 1));
       temp = options[i];
       options[i] = this.state.options[rand];
       options[rand] = temp;
     }
+    // Update state
     this.setState({ options });
   }
 
   reset = () => {
+    // Reset state
     this.setState({
       currPoints: 0,
       guessed: [],
       win: false,
       lose: false
     });
+    // Shuffle characters
     this.shuffle();
-  }
+  };
 
   render() {
     return (
